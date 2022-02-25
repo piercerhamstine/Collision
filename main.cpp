e95826d9e9c8256dd29a88019e0e589924371aaf
Pierce <piercerhamstine@gmail.com>
2022-02-24 22:42:30 -0600

    First Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <random>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 400), "SFML works!");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+            window.clear();
+            
+            window.display();
+    }
+
+    return 0;
+}

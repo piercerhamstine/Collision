b2298c0f0338958bde3822b98c67e96c1fffaa9b
Pierce <piercerhamstine@gmail.com>
2022-03-11 03:58:05 -0600

    Collisions

--- Box/box.hpp
+++ Box/box.hpp
@@ -1,12 +1,16 @@
 #ifndef BOX_HPP
 #define BOX_HPP
 
-class Box
+#include "SFML/Graphics.hpp"
+
+class Box : sf::Drawable
 {
 public:
+    virtual void draw(sf::RenderTarget& rtarget, sf::RenderStates states) const;
     
+    void UpdatePosition(sf::Vector2f position);
 private:
-    
+    sf::RectangleShape rect;
 };
 
 #endif
